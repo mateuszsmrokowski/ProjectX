c329aa6a9161308832db0f06f717d6cbc8e3a163
Mateusz Smrokowski <mateusz.smrokowski@skyrise.tech>
2019-08-13 13:24:45 +0200

    Added void table exceptions

--- Models/Logic/MainBoard/WorkersListGenerator.cs
+++ Models/Logic/MainBoard/WorkersListGenerator.cs
@@ -20,6 +20,9 @@ namespace ProjectX.Models.Logic.MainBoard
             data.WorkersData = new List<WorkersDto> { new WorkersDto { Name = "Ziom1", Surname = "Z2", PhoneNumber = "234432234", Position = "Dir", DayOfWork = new List<DateTime> { DateTime.Now } }  };
             data.WorkersData.Add(new WorkersDto { Name = "Ziom2", Surname = "Z2", PhoneNumber = "234432234", Position = "Knur", DayOfWork = new List<DateTime> { DateTime.Now } });
             data.WorkersData.Add(new WorkersDto { Name = "Ziom3", Surname = "Z3", PhoneNumber = "234432234", Position = "Gbur", DayOfWork = new List<DateTime> { DateTime.Now } });
+            data.WorkersData.Add(new WorkersDto { Name = "Ziom4", Surname = "Z4", PhoneNumber = "234432234", Position = "Bot", DayOfWork = new List<DateTime> { DateTime.Now } });
+            data.WorkersData.Add(new WorkersDto { Name = "Ziom5", Surname = "Z5", PhoneNumber = "234432234", Position = "Kfot", DayOfWork = new List<DateTime> { DateTime.Now } });
+            data.WorkersData.Add(new WorkersDto { Name = "Ziom6", Surname = "Z6", PhoneNumber = "234432234", Position = "Tak :)", DayOfWork = new List<DateTime> { DateTime.Now } });
 
             return data;
         }
